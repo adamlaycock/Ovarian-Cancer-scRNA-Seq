b77eb2514320b87cfcbfdc3449ac3e0b3152bab6
adamlaycock <adam.m.laycock@gmail.com>
2025-05-18 13:37:14 +0100

    add summary statistics

--- analysis_script.py
+++ analysis_script.py
@@ -1,3 +1,13 @@
 import dask.dataframe as dd
 
-dd.read_parquet('Data/parquet_output')
+# Read parquet directory with processed reads
+reads_10x = dd.read_parquet('Data/parquet_output')
+# Verify that structure has changed
+print(f'Reads DataFrame has {reads_10x.shape[1]} columns and {reads_10x.shape[0].compute()} rows.')
+
+# Summary statistics on read data
+gene_total = reads_10x['gene'].nunique().compute()
+average_read = reads_10x['read'].mean().compute()
+std_read = reads_10x['read'].std().compute()
+max_read = reads_10x['read'].max().compute()
+min_read = reads_10x['read'].min().compute()
